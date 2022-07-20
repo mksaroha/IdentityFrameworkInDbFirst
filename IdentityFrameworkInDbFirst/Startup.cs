40f406641706266325872f8b4e42704dcb03355b
mksaroha <mksaroha@hotmail.com>
2022-07-20 17:40:44 +0530

    Add project files.

---
+++ IdentityFrameworkInDbFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityFrameworkInDbFirst.Startup))]
+namespace IdentityFrameworkInDbFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
